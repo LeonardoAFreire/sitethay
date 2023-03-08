5e1c78439d7a1a6a793f2ec7fc625e95098e1ec6
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-03-08 19:47:17 -0300

    menu-mobile

--- js/modulos/btn-mobile.js
+++ js/modulos/btn-mobile.js
@@ -1,40 +1,41 @@
-export default function initBtnMobile() {}
+export default function initBtnMobile() {
+  const btn = document.getElementById('btn-mobile');
+  if (btn) {
+    function toggleMenu(event) {
+      if (event.type === 'touchstart') {
+        event.preventDefault();
+      }
+      const header = document.querySelector('.header-menu');
+      const nav = document.getElementById('nav');
 
-const btn = document.getElementById('btn-mobile');
+      nav.classList.toggle('ativo');
+      header.classList.toggle('ativo');
 
-function toggleMenu(event) {
-  if (event.type === 'touchstart') {
-    event.preventDefault();
-  }
-  const header = document.querySelector('.header-menu');
-  const nav = document.getElementById('nav');
+      const ativo = nav.classList.contains('ativo');
+      event.currentTarget.setAttribute('aria-expanded', ativo);
 
-  nav.classList.toggle('ativo');
-  header.classList.toggle('ativo');
+      if (ativo) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu');
+      } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir menu');
+      }
+    }
 
-  const ativo = nav.classList.contains('ativo');
-  event.currentTarget.setAttribute('aria-expanded', ativo);
+    btn.addEventListener('click', toggleMenu);
+    btn.addEventListener('touchstart', toggleMenu);
 
-  if (ativo) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar menu');
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir menu');
+    const links = document.querySelectorAll('.header-menu a');
+
+    function fecharMenu() {
+      const header = document.querySelector('.header-menu');
+      const nav = document.getElementById('nav');
+
+      nav.classList.remove('ativo');
+      header.classList.remove('ativo');
+    }
+
+    links.forEach((link) => {
+      link.addEventListener('click', fecharMenu);
+    });
   }
 }
-
-btn.addEventListener('click', toggleMenu);
-btn.addEventListener('touchstart', toggleMenu);
-
-const links = document.querySelectorAll('.header-menu a');
-
-function fecharMenu() {
-  const header = document.querySelector('.header-menu');
-  const nav = document.getElementById('nav');
-
-  nav.classList.remove('ativo');
-  header.classList.remove('ativo');
-}
-
-links.forEach((link) => {
-  link.addEventListener('click', fecharMenu);
-});
