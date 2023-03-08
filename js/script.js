5d448af8eaab911d003ac40d11f0ce9f1b60471c
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-03-08 19:46:48 -0300

    menu-mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initScrollSuave from './modulos/scroll-suave.js';
 import initAnimaScroll from './modulos/anima-scroll.js';
+import initBtnMobile from './modulos/btn-mobile.js';
 
 initScrollSuave();
 initAnimaScroll();
+initBtnMobile();
